67615c3899c76bdd828daf31c9a1eb84c9058aa3
gitmanga <gitmanga@github.com>
2018-05-06 12:57:21 +0800

    切换全屏

--- script.js
+++ script.js
@@ -8,6 +8,39 @@ var vm = new Vue({
     methods: {
         ToggleUI: function() {
             this.is_show_ui = !this.is_show_ui
-        }
+        },
+
+        ToggleFullScreen: function() {
+            if (IsFullScreen()){
+                CancelFullScreen()
+            } else {
+                RequestFullScreen(this.$el)
+            }
+        },
     }
 })
+
+function RequestFullScreen(el) {
+    var reqeust = el.requestFullscreen
+        || el.msRequestFullscreen
+        || el.mozRequestFullScreen 
+        || el.webkitRequestFullScreen
+
+    return reqeust ? (reqeust.call(el), true) : false
+}
+
+function CancelFullScreen() {
+    var cancel = document.exitFullscreen
+        || document.msExitFullScreen
+        || document.mozCancelFullScreen
+        || document.webkitCancelFullScreen
+
+    return cancel ? (cancel.call(document), true) : null
+}
+
+function IsFullScreen() {
+    return document.fullscreenElement
+        || document.msFullscreenElement
+        || document.mozFullScreenElement
+        || document.webkitFullscreenElement
+}
